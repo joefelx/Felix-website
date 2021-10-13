d9fe6b03db0b8eae5b6648f7a4df3930c5bd4892
unknown <joefelix.a2003@gmail.com>
2021-10-13 17:42:05 +0530

    commit

--- script.js
+++ script.js
@@ -1,48 +1,61 @@
+const content = document.querySelector(".content");
+let currentPos = window.pageYOffset;
 
+function distort() {
+  const newPos = window.pageYOffset;
+  const diff = newPos - currentPos;
+  const speed = diff * 0.15;
+
+  content.style.transform = "skewY(" + speed + "deg)";
+  currentPos = newPos;
+
+  requestAnimationFrame(distort);
+}
+
+distort();
 
 $(document).ready(() => {
-    $(".nav_icon").click(() => {
-        $(".nav_container").toggle()
-    })
+  $(".nav_icon").click(() => {
+    $(".nav_container").toggle();
+  });
 
-
-    $(".project-heading1").click(() => {
+  $(".project-heading1").click(() => {
+    $(".project-info1").toggle();
+    $("#button1").click(() => {
       $(".project-info1").toggle();
-      $("#button1").click(() => {
-        $(".project-info1").toggle();
-      });
-    })
+    });
+  });
 
-    $(".project-heading2").click(() => {
-      $(".project-info2").toggle();
-      $("#button2").click(() => {
-        $(".project-info2").toggle();
-      });
-    })
-
-    $(".project-heading3").click(() => {
-      $(".project-info3").toggle();
-      $("#button3").click(() => {
-        $(".project-info3").toggle();
-      });
-    })
-
-    $(".project-heading4").click(() => {
-      $(".project-info4").toggle();
-      $("#button4").click(() => {
-        $(".project-info4").toggle();
-      });
-    })
-    
+  $(".project-heading2").click(() => {
+    $(".project-info2").toggle();
     $("#button2").click(() => {
       $(".project-info2").toggle();
     });
-    
+  });
+
+  $(".project-heading3").click(() => {
+    $(".project-info3").toggle();
     $("#button3").click(() => {
       $(".project-info3").toggle();
     });
-    
+  });
+
+  $(".project-heading4").click(() => {
+    $(".project-info4").toggle();
     $("#button4").click(() => {
       $(".project-info4").toggle();
     });
   });
+
+  $("#button2").click(() => {
+    $(".project-info2").toggle();
+  });
+
+  $("#button3").click(() => {
+    $(".project-info3").toggle();
+  });
+
+  $("#button4").click(() => {
+    $(".project-info4").toggle();
+  });
+});
